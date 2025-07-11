ad3acf202aea9345755e88b9f33fc79911522555
Eric Trignau <ertrigna@f6r8s8.clusters.42paris.fr>
2025-07-11 16:18:44 +0200

    push parsing

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 14:10:47 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/07/11 10:53:57 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:18:06 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,8 +122,12 @@ bool	parse_texture(char *line, t_map *map);
 /*PARSE_COLORS*/
 bool	parse_color(char *line, t_map *map);
 
-/*VALID_MAP*/
+/*PARSE_MAP*/
 bool	is_valid_map(t_map *map);
+int		parse_map(char **lines, t_map *map);
+
+/*PRINCIPAL PARSE*/
+int		parse_file(char *filename, t_map *map);
 
 /*INIT*/
 void	init_color(t_color *color);
--- src/parsing/parse_file.c
+++ src/parsing/parse_file.c
@@ -6,9 +6,71 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 11:08:39 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/07/04 11:08:45 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:17:36 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	parse_textures_and_colors(char **lines, t_map *map)
+{
+	int	i;
+	int	parsed_count;
+
+	i = 0;
+	parsed_count = 0;
+	while (lines[i] && parsed_count < 6)
+	{
+		if (is_texture(lines[i]))
+		{
+			if (!parse_texture(lines[i], map))
+				return (ft_putstr_fd("Error: Invalid texture\n", 2), 0);
+			parsed_count++;
+		}
+		else if (is_color(lines[i]))
+		{
+			if (!parse_color(lines[i], map))
+				return (ft_putstr_fd("Error: Invalid color\n", 2), 0);
+			parsed_count++;
+		}
+		i++;
+	}
+	if (parsed_count == 6)
+		return (1);
+	return (0);
+}
+
+static int	validate_parsing(t_map *map)
+{
+	if (!map->texture.no || !map->texture.so || 
+		!map->texture.ea || !map->texture.we)
+		return (ft_putstr_fd("Error: Missing textures\n", 2), 0);
+	return (1);
+}
+
+int	parse_file(char *filename, t_map *map)
+{
+	char	**lines;
+
+	lines = read_files(filename);
+	if (!lines)
+		return (ft_putstr_fd("Error: Cannot read file\n", 2), 0);
+	init_map(map);
+	if (!parse_textures_and_colors(lines, map))
+	{
+		ft_free_tab(lines);
+		return (0);
+	}
+	if (!parse_map(lines, map))
+	{
+		ft_free_tab(lines);
+		return (0);
+	}
+	if (!validate_parsing(map))
+	{
+		ft_free_tab(lines);
+		return (0);
+	}
+	ft_free_tab(lines);
+	return (1);
+}

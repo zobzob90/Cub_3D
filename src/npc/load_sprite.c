d588618c58eb5be40e8e2657c532f25c1b7454d4
Eric Trignau <ertrigna@f6r7s1.clusters.42paris.fr>
2025-07-23 17:38:23 +0200

    norme load_sprite.c ok

--- src/npc/load_sprite.c
+++ src/npc/load_sprite.c
@@ -6,7 +6,7 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 17:26:32 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/07/23 17:32:50 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/07/23 17:38:01 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 static void	load_first_sprite(t_game *game)
 {
 	game->sprite[0].img = mlx_xpm_file_to_image(game->mlx,
-		"./texture/xpm/npc/pig_walking.xpm",
-		&game->sprite[0].width, &game->sprite[0].height);
+			"./texture/xpm/npc/pig_walking.xpm",
+			&game->sprite[0].width, &game->sprite[0].height);
 	if (game->sprite[0].img)
 	{
 		game->sprite[0].img_data = mlx_get_data_addr(game->sprite[0].img,
@@ -27,22 +27,25 @@ static void	load_first_sprite(t_game *game)
 
 static void	load_second_sprite(t_game *game)
 {
-	game->sprite[1].img = mlx_xpm_file_to_image(game->mlx, "./texture/xpm/npc/pig_walking_2.xpm", 
-		&game->sprite[1].width, &game->sprite[1].height);
+	game->sprite[1].img = mlx_xpm_file_to_image(game->mlx,
+			"./texture/xpm/npc/pig_walking_2.xpm",
+			&game->sprite[1].width, &game->sprite[1].height);
 	if (game->sprite[1].img)
 	{
-		game->sprite[1].img_data = mlx_get_data_addr(game->sprite[1].img, 
-			&game->sprite[1].bpp, &game->sprite[1].size_line, &game->sprite[1].endian);
+		game->sprite[1].img_data = mlx_get_data_addr(game->sprite[1].img,
+				&game->sprite[1].bpp, &game->sprite[1].size_line,
+				&game->sprite[1].endian);
 	}
 }
 
 static void	load_third_sprite(t_game *game)
 {
-	game->sprite[2].img = mlx_xpm_file_to_image(game->mlx, "./texture/xpm/npc/pig_walking_3.xpm", 
+	game->sprite[2].img = mlx_xpm_file_to_image(game->mlx,
+			"./texture/xpm/npc/pig_walking_3.xpm",
 			&game->sprite[2].width, &game->sprite[2].height);
 	if (game->sprite[2].img)
 	{
-		game->sprite[2].img_data = mlx_get_data_addr(game->sprite[2].img, 
+		game->sprite[2].img_data = mlx_get_data_addr(game->sprite[2].img,
 				&game->sprite[2].bpp,
 				&game->sprite[2].size_line, &game->sprite[2].endian);
 	}
